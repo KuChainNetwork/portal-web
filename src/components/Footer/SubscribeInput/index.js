8ee67bc054fc4d356859b2cb3d43c1a0f0ceaeb8
qhxin2 <vitace.luo@kuchain.io>
2020-03-30 13:35:53 +0800

    subscribe

--- src/components/Footer/SubscribeInput/index.js
+++ src/components/Footer/SubscribeInput/index.js
@@ -1,21 +1,39 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import { Input, Button } from 'antd';
 import styles from './style.less';
 
 const SubscribeInput = () => {
+  const [value, setValue] = useState(undefined);
+
+  const handleChange = useCallback((e) => {
+    setValue(e.target.value);
+  }, []);
+
   return (
-    <div className={styles.input}>
+    <form
+      className={styles.input}
+      action="https://network.us19.list-manage.com/subscribe/post?u=62053eb433ce5c9c4f226590f&amp;id=1bd3042b54"
+      method="POST"
+      name="mc-embedded-subscribe-form"
+      target="_blank"
+    >
       <Input
         size="large"
         placeholder="Input Your Email"
+        name="EMAIL"
+        value={value}
+        onChange={handleChange}
       />
+      <input type="hidden" name="b_62053eb433ce5c9c4f226590f_1bd3042b54" tabIndex="-1" value="" />
       <Button
         size="large"
         type="primary"
+        htmlType="submit"
+        name="subscribe"
       >
         Subscribe
       </Button>
-    </div>
+    </form>
   );
 };
 
