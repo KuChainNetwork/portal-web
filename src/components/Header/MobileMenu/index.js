11be5dea9003a1e0e522980749ee64bdae9a73d2
tigerHee <tigerhee@tigerHeedeMacBook-Pro.local>
2020-04-07 10:13:59 +0800

    mobile header

---
+++ src/components/Header/MobileMenu/index.js
@@ -0,0 +1,106 @@
+import React, { useState, useImperativeHandle } from 'react';
+import styles from './style.less';
+import { Drawer } from 'antd';
+import classname from 'classname';
+import { Link } from 'components/Router';
+import { connect } from 'dva';
+
+const MobileMenu = props => {
+  const { rightMenus, pathname, cRef, langs, currentLang, dispatch } = props;
+  const [show, setShow] = useState(false);
+  const [langShow, setLangShow] = useState(false);
+
+  const _show = () => {
+    setShow(true);
+  };
+
+  const _close = () => {
+    setShow(false);
+  };
+
+  useImperativeHandle(cRef, () => ({
+    _show,
+  }));
+
+  const _langShow = () => {
+    setLangShow(true);
+  };
+
+  const _langclose = () => {
+    setLangShow(false);
+  };
+  
+  const _selectLang = val => {
+    dispatch({
+      type: 'app/selectLang',
+      payload: {
+        lang: val,
+      },
+    });
+    _langclose();
+  };
+
+  return (
+    <Drawer
+      className={styles['menuDrawer']}
+      placement="right"
+      closable={true}
+      onClose={_close}
+      visible={show}
+    >
+      <div className={styles['menuContent']}>
+        {rightMenus.map(item => {
+          const { path, title, pc, icon } = item;
+          const cls = classname({
+            [styles.menu]: true,
+            [styles.active]: pathname === path,
+          });
+          return pc ? null : (
+            <Link onClick={_close} key={path} to={path} className={cls}>
+              <img src={require(`assets/header/${icon}.svg`)} alt="" />
+              <span>{title}</span>
+            </Link>
+          );
+        })}
+        <div onClick={_langShow} className={styles['menu']}>
+          <img src={require(`assets/header/icon_lang.svg`)} alt="" />
+          <span>{langs.find(item => item.key === currentLang).label}</span>
+        </div>
+        <Drawer
+          className={styles['langDrawer']}
+          width="100%"
+          title="选择语言"
+          placement="right"
+          onClose={_langclose}
+          visible={langShow}
+        >
+          <div className={styles['slectLang']}>
+            {langs.map(item => (
+              <div
+                onClick={() => {
+                  _selectLang(item.key);
+                }}
+                className={styles['slectLang-item']}
+                key={item.key}
+              >
+                <div className={styles['slectLang-item-left']}>{item.label}</div>
+                <div className={styles['slectLang-item-right']}>
+                  {item.key === currentLang && (
+                    <img src={require(`assets/header/icon_select.svg`)} alt="" />
+                  )}
+                </div>
+              </div>
+            ))}
+          </div>
+        </Drawer>
+      </div>
+    </Drawer>
+  );
+};
+
+export default connect(state => {
+  return {
+    langs: state.app.langs,
+    currentLang: state.app.currentLang,
+  };
+})(MobileMenu);
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,23 +1,34 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import _ from 'lodash';
 import classname from 'classname';
 import { Link } from 'components/Router';
 import Responsive from 'components/Responsive';
 import LangSelector from './LangSelector';
+import MobileMenu from './MobileMenu';
 import { _t } from 'utils/lang';
 import styles from './style.less';
 import logo from 'assets/logo.svg';
+import mMenuIcon from 'assets/header/icon_menu.svg';
 
 // TODO V0.2 sidebar
 const Header = ({ pathname }) => {
+  const mobileMenuRef = useRef(null);
   const rightMenus = [
+    {
+      title: '首页',
+      path: '/',
+      icon: 'icon_index',
+      mobile: true,
+    },
     {
       title: _t('head.menu.whykuchain'),
       path: '/why-kuchain',
+      icon: 'icon_why',
     },
     {
       title: _t('head.menu.news'),
       path: '/news',
+      icon: 'icon_news',
     },
     // {
     //   title: _t('head.menu.community'),
@@ -26,6 +37,7 @@ const Header = ({ pathname }) => {
     {
       title: _t('head.menu.blog'),
       path: '/blog',
+      pc: true,
     },
   ];
 
@@ -33,7 +45,6 @@ const Header = ({ pathname }) => {
     <div className={styles.affix}>
       <div className={styles.head}>
         <div className={styles.limit}>
-
           <div className={styles.headLeft}>
             <Link to="/" className={styles.logo}>
               <img src={logo} alt="KuChain" />
@@ -42,13 +53,15 @@ const Header = ({ pathname }) => {
           <div className={styles.headRight}>
             <Responsive>
               <React.Fragment>
-                {_.map(rightMenus, ({ path, title }) => {
+                {_.map(rightMenus, ({ path, title, mobile }) => {
                   const cls = classname({
                     [styles.menu]: true,
                     [styles.active]: pathname.indexOf(path) === 0,
                   });
-                  return (
-                    <Link key={path} to={path} className={cls}>{ title }</Link>
+                  return mobile ? null : (
+                    <Link key={path} to={path} className={cls}>
+                      {title}
+                    </Link>
                   );
                 })}
               </React.Fragment>
@@ -60,7 +73,15 @@ const Header = ({ pathname }) => {
 
             <Responsive.Mobile>
               <div className={styles.langSelectMb}>
-                <LangSelector />
+                {/* <LangSelector /> */}
+                <div
+                  onClick={() => {
+                    mobileMenuRef.current._show();
+                  }}
+                >
+                  <img src={mMenuIcon} alt="" />
+                </div>
+                <MobileMenu rightMenus={rightMenus} pathname={pathname} cRef={mobileMenuRef} />
               </div>
             </Responsive.Mobile>
           </div>
